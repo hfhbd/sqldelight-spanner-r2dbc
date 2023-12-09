436d7385c85077b0e7cf0b8ba6913d116f5f5683
hfhbd <hfhbd@users.noreply.github.com>
2023-12-09 16:13:21 +0100

    Bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,17 @@
 plugins {
-    kotlin("jvm") version "1.7.22"
-    id("app.cash.sqldelight") version "2.0.0-alpha04"
-    id("com.google.cloud.artifactregistry.gradle-plugin") version "2.2.0"
-    application
+    kotlin("jvm") version "1.9.21"
+    id("app.cash.sqldelight") version "2.0.1"
+    // id("com.google.cloud.artifactregistry.gradle-plugin") version "2.2.0"
+    id("application")
 }
 
 dependencies {
-    implementation("app.cash.sqldelight:r2dbc-driver:2.0.0-alpha04")
-    implementation("app.cash.sqldelight:async-extensions:2.0.0-alpha04")
-    
+    implementation("app.cash.sqldelight:r2dbc-driver:2.0.1")
+    implementation("app.cash.sqldelight:async-extensions:2.0.1")
+
     implementation("com.google.cloud:cloud-spanner-r2dbc:1.2.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.4")
-    
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.7.1")
+
     implementation("org.slf4j:slf4j-simple:2.0.6")
     testImplementation(kotlin("test"))
 }
@@ -21,10 +21,10 @@ kotlin {
 }
 
 sqldelight {
-    database("Spanner") {
-        dialect("app.cash.sqldelight:postgresql-dialect:2.0.0-alpha04")
-        packageName = "app.softwork.spanner"
-        generateAsync = true
+    databases.register("Spanner") {
+        dialect("app.cash.sqldelight:postgresql-dialect:2.0.1")
+        packageName.set("app.softwork.spanner")
+        generateAsync.set(true)
     }
 }
 
