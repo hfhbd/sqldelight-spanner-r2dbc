5395339b9e3158cc03d1f2325bcee333d1e62904
hfhbd <22521688+hfhbd@users.noreply.github.com>
2024-09-04 20:05:30 +0200

    Update build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,28 @@
 plugins {
-    kotlin("jvm") version "1.9.21"
-    id("app.cash.sqldelight") version "2.0.1"
-    // id("com.google.cloud.artifactregistry.gradle-plugin") version "2.2.0"
-    id("application")
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.licensee)
+    alias(libs.plugins.sqldelight)
+    application
 }
 
 dependencies {
-    implementation("app.cash.sqldelight:r2dbc-driver:2.0.1")
-    implementation("app.cash.sqldelight:async-extensions:2.0.1")
+    implementation(libs.sqldelight.r2dbc.driver)
+    implementation(libs.sqldelight.async.extensions)
 
-    implementation("com.google.cloud:cloud-spanner-r2dbc:1.2.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.7.1")
+    implementation(libs.cloud.spanner.r2dbc)
+    implementation(libs.coroutines.reactor)
 
-    implementation("org.slf4j:slf4j-simple:2.0.6")
+    implementation(libs.logback)
     testImplementation(kotlin("test"))
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
 
 sqldelight {
     databases.register("Spanner") {
-        dialect("app.cash.sqldelight:postgresql-dialect:2.0.1")
+        dialect(libs.sqldelight.postgresql.dialect)
         packageName.set("app.softwork.spanner")
         generateAsync.set(true)
     }
